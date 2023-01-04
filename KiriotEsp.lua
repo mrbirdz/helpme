edd7c77ab5ea70e0479f231c77993af9cf87c9bc
mrbirdz <71379363+mrbirdz@users.noreply.github.com>
2023-01-04 17:57:57 +0400

    Update KiriotEsp.lua

--- KiriotEsp.lua
+++ KiriotEsp.lua
@@ -1,17 +1,17 @@
 --Settings--
 local ESP = {
-    Enabled = true,
-    Boxes = false,
+    Enabled = false,
+    Boxes = true,
     BoxShift = CFrame.new(0,-1.5,0),
-    BoxSize = Vector3.new(4,6,0),
+	BoxSize = Vector3.new(4,6,0),
     Color = Color3.fromRGB(255, 170, 0),
     FaceCamera = false,
-    Names = false,
+    Names = true,
     TeamColor = true,
     Thickness = 2,
     AttachShift = 1,
     TeamMates = true,
-    Players = false,
+    Players = true,
     
     Objects = setmetatable({}, {__mode="kv"}),
     Overrides = {}
